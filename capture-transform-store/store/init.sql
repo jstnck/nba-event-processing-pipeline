1c3505e3ec6a757e2e9a7cefd8605c544c06848f
Justin <me@example>
2021-07-27 22:00:12 -0400

    adding debezium / connect

--- capture-transform-store/store/init.sql
+++ capture-transform-store/store/init.sql
@@ -1,8 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE frame_data (
-    id SERIAL,
+    id uuid DEFAULT uuid_generate_v4 (),
     file_name VARCHAR(255),
     fps INT,
     frame_num INT,
     frame_array JSONB,
     PRIMARY KEY (file_name, frame_num)
-)
+);
